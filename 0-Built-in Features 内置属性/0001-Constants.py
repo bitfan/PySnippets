c30abcbde83a8f7d3701dc13f2c9e2f4ec5f2950
WangPlusPlus <chancewang@me.com>
2020-07-16 14:44:41 -0400

    更新常量

--- 0-Built-in Features 内置属性/0001-Constants.py
+++ 0-Built-in Features 内置属性/0001-Constants.py
@@ -4,3 +4,10 @@ constants = [False, True, None, NotImplemented, Ellipsis, __debug__]
 # False and True
 
 # None
+
+# NotImplemented
+
+# Ellipsis
+'''
+The same as the ellipsis literal “...”. Special value used mostly in conjunction with extended slicing syntax for user-defined container data types.
+'''
